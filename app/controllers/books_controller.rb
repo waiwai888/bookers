56b6a9b60f8c8242cf69c2980e0fcb8f0c6dee56
waiwai888 <yuisato9688@gmail.com>
2021-06-10 00:45:37 +0000

    [3-8]投稿後showページへ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,18 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
-  end
-  
-  def new
     @book = Book.new
   end
 
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+  
+private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_050221) do
+ActiveRecord::Schema.define(version: 2021_06_09_103325) do
 
   create_table "bookers", force: :cascade do |t|
     t.string "title"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2021_06_09_050221) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
